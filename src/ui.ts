bc02dcf3fa43384f92ca9be2b3895c834a43173a
antash-mishra <mishraantash34@gmail.com>
2025-04-29 20:55:13 +0530

    add random generation frunction which is used in simplex noise which creates same world using seed function

--- src/ui.ts
+++ src/ui.ts
@@ -4,10 +4,15 @@ import { World } from "./world";
 export function createUI(world: World) {
   const gui = new GUI();
 
-  gui.add(world.size, 'width', 8, 128, 1);
-  gui.add(world.size, 'height', 8, 128, 1);
-  gui.add(world, 'generate');
+  gui.add(world.size, 'width' as keyof typeof world.size, 8, 128, 1).name('width');
+  gui.add(world.size, 'height' as keyof typeof world.size, 8, 128, 1).name('height');
 
+  const terrainFolder = gui.addFolder('Terrain');
+  terrainFolder.add(world.params, 'seed', 0, 10000).name('seed');
+  terrainFolder.add(world.params.terrain, 'scale', 10, 100).name('scale');
+  terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('magnitude');
+  terrainFolder.add(world.params.terrain, 'offset', 0, 1).name('offset');
+  
   gui.onChange(() => {
     world.generate();
   });
